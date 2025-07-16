669ffe15afba797f26987e2ba5666a2372714229
SanjeevanUthayachandran <e21356@eng.pdn.ac.lk>
2025-07-16 22:08:44 +0530

    last

--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -1,5 +1,6 @@
 package com.example.demo.student;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 
@@ -9,16 +10,15 @@ import java.util.List;
 
 @Component
 public class StudentService {
-    public List<Student> getStudents() {
-        return List.of(
-                new Student(
-                        1L,
-                        "Marism",
-                        "marisam@gmail.com",
-                        LocalDate.of(2000, Month.JANUARY, 5),
-                        21
-                )
 
-        );
+    private final StudentRepository studentRepository;
+
+    @Autowired
+    public StudentService(StudentRepository studentRepository) {
+        this.studentRepository = studentRepository;
+    }
+
+    public List<Student> getStudents() {
+        return studentRepository.findAll();
     }
 }
