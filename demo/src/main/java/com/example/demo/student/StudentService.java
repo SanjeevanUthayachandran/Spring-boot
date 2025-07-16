18d8b9f7c34888ce738fe7a2b88c1f2e36f02a60
SanjeevanUthayachandran <e21356@eng.pdn.ac.lk>
2025-07-16 20:24:03 +0530

    connect database

---
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -0,0 +1,24 @@
+package com.example.demo.student;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Component
+public class StudentService {
+    public List<Student> getStudents() {
+        return List.of(
+                new Student(
+                        1L,
+                        "Marism",
+                        "marisam@gmail.com",
+                        LocalDate.of(2000, Month.JANUARY, 5),
+                        21
+                )
+
+        );
+    }
+}
